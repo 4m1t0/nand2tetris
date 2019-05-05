d4fb3b47378ab3c47cd8b94cded553e4fa4abb01
4m1t0 <fum1t0.eng@gmail.com>
2019-05-05 13:13:56 +0900

    implement Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,47 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(SETUP)
+    @SCREEN
+    D=A
+    @pixel
+    M=D    // set the first SCREEN address (16384)
+
+    @KBD
+    D=M
+    @IFPRESS
+    D;JGT
+    @ELSE
+    0;JMP
+(IFPRESS)
+    @color
+    M=-1
+    @ENDIF
+    0;JMP
+(ELSE)
+    @color
+    M=0
+(ENDIF)
+
+(FILL)
+    @color
+    D=M
+    @pixel
+    A=M
+    M=D
+    @pixel
+    M=M+1
+
+    @8192   // 256 rows * 32 words = 8192
+    D=A
+    @SCREEN
+    D=D+A
+    @pixel
+    D=D-M
+
+    @FILL
+    D;JGT
+
+    @SETUP
+    0;JMP   // infinite loop
